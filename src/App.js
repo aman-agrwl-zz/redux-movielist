7f4f49496e7be0eb70e5f5579aeca4654dcb7ac5
Aman Agrawal <aman.agrawal@ad.loginradius.net>
2020-08-19 10:37:12 +0530

    Added sample redux project for learnign purpose

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {connect} from 'react-redux';
+import updateMovies from './store/actions/updateMovies';
+import fetchUsers from './store/actions/fetchUsers';
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>REDUX MOVIELIST APP</h3>
+      <br/>
+       <span style={{color:'green'}}>Your current movie is: {props.movies.name}</span>
+       <br/>
+       <button onClick={props.updateMovies}>Select Movies</button>
+       <br/>
+       {props.users.length === 0 ?
+       <p>THERE ARE NO USERS</p>:
+       props.users.map(user => <p key={user.id}>{user.first_name} -    {user.email}</p>)}
+         <br/>
+    <button onClick={props.fetchUsers}>FETCH USERS</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { 
+    movies: state.movies,
+    users: state.users
+  }
+};
+
+const MapDispatchToProps = (dispatch) => {
+  return { 
+    updateMovies: ()=> dispatch(updateMovies),
+    fetchUsers: ()=> dispatch(fetchUsers)
+  }
+}
+
+export default connect(mapStateToProps, MapDispatchToProps)(App);
